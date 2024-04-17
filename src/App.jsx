1300477c382893c9dc4abe5800d23f70731883d8
VanshSahay <vansh.sahay1234@gmail.com>
2024-04-17 16:29:22 +0530

    Initial setup, added tailwind and file structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar, Page, Sidebar } from './Components'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Page/>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default App
